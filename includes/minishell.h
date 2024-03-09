fc55214907d054d6678a45140472fd01f7ebe799
Haruto Miyazaki <hmiyazak@c1r11s1.42tokyo.jp>
2024-03-09 12:01:35 +0900

    creating basic actions of shell

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,17 +6,58 @@
 /*   By: hmiyazak <hmiyazak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 19:54:00 by hmiyazak          #+#    #+#             */
-/*   Updated: 2023/12/01 20:22:54 by hmiyazak         ###   ########.fr       */
+/*   Updated: 2024/03/09 11:35:24 by hmiyazak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
 # include <readline/readline.h>
 # include <readline/history.h>
 # include <stdbool.h>
+# include <limits.h>
+# include <string.h>
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include "../libft/libft.h"
+
+typedef enum e_token
+{
+	PIPE,
+	SPASE,
+	S_QUOTE,
+	D_QUOTE,
+	STR
+}						t_token;
+
+typedef struct s_node
+{
+	int				data;
+	struct s_node	*prev;
+	struct s_node	*next;
+	char			*str;
+}	t_node;
+
+typedef struct s_stack
+{
+	t_node	*top;
+	t_node	*bottom;
+	int		current;
+}	t_stack;
+
+void	signalctrl(void);
+char	*ft_strtrim(char const *s1, char const *set);
+char	*split_quote(t_stack *lexer, char *line);
+char	*split_normal(t_stack *lexer, char *line);
+char	*split_line(t_stack *lexer, char *line);
+t_stack	lexer(char *line);
+t_stack	*create_stack(void);
+void	push(t_stack *stack, char *i, int type);
+int		pop(t_stack *stack);
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -5,24 +5,140 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hmiyazak <hmiyazak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/01 19:53:36 by hmiyazak          #+#    #+#             */
-/*   Updated: 2023/12/01 20:25:58 by hmiyazak         ###   ########.fr       */
+/*   Created: 2024/01/29 10:21:14 by yususato          #+#    #+#             */
+/*   Updated: 2024/03/09 11:46:29 by hmiyazak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	main(void)
+void	put_error_exit(char *error)
+{
+	perror(error);
+	exit(1);
+}
+
+char	*get_pass(char *line)
+{
+	char	path[PATH_MAX];
+	char	*str;
+	char	*end;
+	char	*copy;
+
+	str = getenv("PATH");
+	while (*str)
+	{
+		bzero(path, PATH_MAX);
+		end = strchr(str, ':');
+		if (end)
+			strncpy(path, str, end - str);
+		else
+			strlcpy(path, str, end - str);
+		strlcat(path, "/", PATH_MAX);
+		strlcat(path, line, PATH_MAX);
+		if (access(path, X_OK) == 0)
+		{
+			copy = strdup(path);
+			if (copy == NULL)
+				exit(0);
+			return (copy);
+		}
+		if (end == NULL)
+			return (NULL);
+		str = end + 1;
+	}
+	return (NULL);
+}
+
+int	main(int argc, char **argv, char **env)
 {
 	char	*line;
+	char	**args;
+	pid_t	pid;
+	int		status;
 
+	signalctrl();
 	while (true)
 	{
 		line = readline("> ");
-		printf("%s\n", line);
+		if (!line)
+			break ;
+		else
+		{
+			add_history(line);
+			pid = fork();
+			if (pid < 0)
+				put_error_exit("failed to fork");
+			else if (pid == 0)
+			{
+				exit(0);
+			}
+			else
+			{
+				wait(&status);
+				printf("%d\n", status);
+			}
+			free(line);
+		}
 	}
 	exit(0);
 }
-// __attribute((destructor)) static void destructor() {
-// 	system("leaks -q minishell");
+
+// int	main(int ac, char **av, char **env)
+// {
+// 	char	*line;
+// 	t_stack	stack;
+// 	t_stack	sum;
+// 	t_node	*node;
+// 	pid_t	pid;
+// 	int		status;
+// 	char	*path;
+
+// 	//using_history();
+// 	signal(SIGINT, sig_handler);
+// 	while (true)
+// 	{
+// 		line = readline("> ");
+// 		if (!line)
+// 			break ;
+// 		if (line)
+// 		{
+// 			add_history(line);
+// 			path = get_pass(line);
+// 			pid = fork();
+// 			if (pid < 0)
+// 			{
+// 				perror("fork");
+// 				exit(0);
+// 			}
+// 			else if (pid == 0)
+// 			{
+// 				char *args[] = {line, NULL};
+				
+// 				if (execve(path, args, env) == -1)
+// 				{
+// 					perror("execve");
+// 					exit(0);
+// 				}
+// 			}
+// 				else
+// 					wait(&status);
+// 			printf("%d\n",status);
+// 			free(line);
+			
+			
+// 			// stack = lexer(line);
+// 			// node = stack.top;
+// 			// node->next = NULL;
+// 			// while (node->prev != NULL)
+// 			// 	node = node->prev;
+// 			// parser(node);
+
+// 		}
+// 	}
+// 	exit(0);
 // }
+
+// // __attribute((destructor)) static void destructor() {
+// // 	system("leaks -q minishell");
+// // }
