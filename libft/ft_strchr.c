05f67acb4aad625147177ddb7de2130c2530ce9a
Haruto Miyazaki <hmiyazak@c4r2s4.42tokyo.jp>
2024-04-29 12:21:50 +0900

    removed unnecessary main function in strchr

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -3,15 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yususato <yususato@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hmiyazak <hmiyazak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 12:07:20 by yususato          #+#    #+#             */
-/*   Updated: 2023/11/20 16:18:31 by yususato         ###   ########.fr       */
+/*   Updated: 2024/04/29 12:21:25 by hmiyazak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 char	*ft_strchr(const char *s, int c)
 {
@@ -25,21 +24,3 @@ char	*ft_strchr(const char *s, int c)
 		return ((char *)s);
 	return (NULL);
 }
-
-int main()
-{
-	char c[] = "abc.ber";
-	char d = '.';
-	char *a;
-
-	a = strrchr(c,d);
-	if (a != NULL)
-	{
-		if (strncmp(a,".ber",5) != 0)
-		;
-	}
-	exit(1);
-	
-	
-}
-
