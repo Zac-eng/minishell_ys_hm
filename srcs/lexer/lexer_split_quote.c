79318019acd4aa1f00d153cfae66906e4902f0c0
Haruto Miyazaki <hmiyazak@c1r5s8.42tokyo.jp>
2024-06-13 21:45:10 +0900

    done separating malloc section in ft_split_quote

--- srcs/lexer/lexer_split_quote.c
+++ srcs/lexer/lexer_split_quote.c
@@ -6,75 +6,76 @@
 /*   By: hmiyazak <hmiyazak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 11:12:37 by hmiyazak          #+#    #+#             */
-/*   Updated: 2024/06/13 11:36:11 by hmiyazak         ###   ########.fr       */
+/*   Updated: 2024/06/13 21:43:36 by hmiyazak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static char	*allocate_quoted(char *line, char quote_kind);
+
 t_token	*split_squote(char **tmp, char *line)
 {
-	int		start;
-	int		end;
+	int		index;
 	char	*set;
 
-	start = 0;
-	end = 0;
-	while (line[start])
-	{
-		if (line[start] == '\'')
-			break ;
-		start++;
-	}
-	set = (char *)malloc(sizeof(char) * (start + 1));
+	index = 0;
+	set = allocate_quoted(line, '\'');
 	if (set == NULL)
-		put_error_exit("need to be editted");
-	end = start;
-	start = 0;
-	line++;
-	while (start <= end && *line)
+		return (NULL);
+	while (line[index + 1] != '\0')
 	{
-		if (*line == '\'')
+		if (line[index + 1] == '\'')
 			break ;
-		set[start] = *line;
-		start++;
-		line++;
+		set[index] = line[index + 1];
+		index += 1;
 	}
-	set[end] = '\0';
-	*tmp = line;
+	set[index] = '\0';
+	if (line[index + 1] == '\'')
+		*tmp = &line[index + 2];
+	else
+		*tmp = &line[index + 1];
 	return (create_token(set, TK_SQUOTE));
 }
 
 t_token	*split_dquote(char **tmp, char *line)
 {
-	int		start;
-	int		end;
+	int		index;
 	char	*set;
 
-	start = 1;
-	end = 0;
-	while (line[start])
-	{
-		if (line[start] == '\"')
-			break ;
-		start++;
-	}
-	set = (char *)malloc(sizeof(char) * (start));
+	index = 0;
+	set = allocate_quoted(line, '\"');
 	if (set == NULL)
-		put_error_exit("need to be editted");
-	end = start;
-	start = 0;
-	line++;
-	while (start <= end && *line)
+		return (NULL);
+	while (line[index + 1] != '\0')
 	{
-		if (*line == '\"')
+		if (line[index + 1] == '\"')
 			break ;
-		set[start] = *line;
-		start++;
-		line++;
+		set[index] = line[index + 1];
+		index += 1;
 	}
-	set[end] = '\0';
-	*tmp = line;
+	set[index] = '\0';
+	if (line[index + 1] == '\"')
+		*tmp = &line[index + 2];
+	else
+		*tmp = &line[index + 1];
 	return (create_token(set, TK_DQUOTE));
 }
 
+static char	*allocate_quoted(char *line, char quote_kind)
+{
+	char	*quoted;
+	int		len;
+
+	len = 0;
+	if (line == NULL || (quote_kind != '\'' && quote_kind != '\"'))
+		return (NULL);
+	if (line[0] != quote_kind)
+		return (NULL);
+	while (line[len + 1] != '\0' && line[len + 1] != quote_kind)
+		len += 1;
+	quoted = (char *)malloc(sizeof(char) * (len + 1));
+	if (quoted == NULL)
+		exit(1);
+	return (quoted);
+}
